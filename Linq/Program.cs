bfa4a6fc0ce0401e92ad2580fef1b1d338744efb
Jordi <jordi.contreras@gmail.com>
2020-05-13 15:07:06 +0200

    Fase 1 ejercicio Linq

---
+++ Linq/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace Linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //********** FASE 1 **********
+            Console.WriteLine("********** FASE 1 **********");
+            int[] arrayNumbers = new int[15] { 2, 6, 8, 4, 5, 5, 9, 2, 1, 8, 7, 5, 9, 6, 4 };
+            
+            //Realizamos la query forzando la ejecución para que guarde en un array
+            var evenNumsQuery = 
+            (from num in arrayNumbers
+            where (num % 2) == 0
+            select num).ToArray();
+
+            //Imprimimos por pantalla los valores obtenidos del array de números pares
+            foreach(int val in evenNumsQuery)
+            {
+                Console.WriteLine(val);
+            }
+
+        }
+    }
+}
